8b554ca37eefd937e6f39702f2b713ca2cf703f7
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-12-23 14:35:36 +0700

    chore: resolve dependencies

---
+++ contest-service/src/main/java/com/daonq1408/contestservice/ContestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daonq1408.contestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestServiceApplication.class, args);
+    }
+
+}
